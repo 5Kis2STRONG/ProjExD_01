26d0d763dfd559454f3df589d681c2be75df3aa0
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-04-18 16:41:09 +0900

    演習終了

--- pygame_image.py
+++ pygame_image.py
@@ -6,6 +6,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_rev = pg.transform.flip(bg_img, True, False)
+    bg_rot = [bg_img, bg_rev]
     koka_img = pg.image.load("ex01/fig/3.png")
     koka_rev = pg.transform.flip(koka_img, True, False)
     koka_25 = pg.transform.rotozoom(koka_rev, 2.5, 1.0)
@@ -22,14 +24,14 @@ def main():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        if x>=1599:
-            x=0
         if y>=120:
             y=0
+        if x>=3199:
+            x=0
         x+=1
-        y+=1
         screen.blit(bg_img, [-x,0])
-        screen.blit(bg_img,[1600-x,0])
+        screen.blit(bg_rev,[1600-x,0])
+        screen.blit(bg_img, [3200-x, 0])
         if y<=19:
             screen.blit(koka_byousha[0], [300, 200])
         elif y<=39:
@@ -45,7 +47,7 @@ def main():
 
 
         pg.display.update()
-        clock.tick(100)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
