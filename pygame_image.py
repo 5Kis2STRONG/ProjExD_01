58d818f00a0f50dac8f884fe25552f3353334453
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-04-18 15:52:07 +0900

    練習一問目終了

--- pygame_image.py
+++ pygame_image.py
@@ -7,12 +7,13 @@ def main():
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     koka_img = pg.image.load("ex01/fig/3.png")
-    kokat_rev = pg.transform.flip(koka_img, True, False)
-    kokat_10 = pg.transform.rotozoom(koka_img, 10, 1.0)
-    kokat_byousha = [kokat_rev, kokat_10]
+    koka_rev = pg.transform.flip(koka_img, True, False)
+    koka_10 = pg.transform.rotozoom(koka_rev, 10, 1.0)
+    koka_byousha = [koka_rev, koka_10]
 
     tmr = 0
     x = 0
+    y=0
 
     while True:
         for event in pg.event.get():
@@ -21,9 +22,17 @@ def main():
         tmr += 1
         if x>=1599:
             x=0
+        if y>=100:
+            y=0
         x+=1
+        y+=1
         screen.blit(bg_img, [-x,0])
         screen.blit(bg_img,[1600-x,0])
+        if y<=49:
+            screen.blit(koka_byousha[0], [300, 200])
+        if y>=50:
+            screen.blit(koka_byousha[1], [300, 200])
+
 
         pg.display.update()
         clock.tick(100)
