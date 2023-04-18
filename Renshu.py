24218a7610e1e26f33a6a7fd1cb89a42482a279e
5Kis2STRONG <c0a22114d7@edu.teu.ac.jp>
2023-04-18 15:05:22 +0900

    練習一問目

---
+++ Renshu.py
@@ -0,0 +1,8 @@
+import pygame as pg
+
+screen = pg.display.set_mode((1600,900))
+bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+koka_img = pg.image.load("ex01/fig/3.png")
+kokat_rev = pg.transform.flip(koka_img, True, False)
+kokat_10 = pg.transform.rotozoom(koka_img, 10, 1.0)
+kokat_byousha = [kokat_rev, kokat_10]
